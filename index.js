a88ae12e50e5fe536bf0ec654023b3458ecfcdbc
youssef4nasser <youssef4nasser@gmail.com>
2023-10-15 13:05:32 +0300

    vercel

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.use(cors())
 
 app.use(express.json())
 
+app.use("/", (req, res,)=>{
+    console.log("Welcome")
+})
+
 bootstrap(app)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
